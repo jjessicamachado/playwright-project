4c7b15774b21d7a5c083dfefabcf1a1afe7a06e7
Felipe Souza de Amorim <jessicasmachado@live.com>
2026-06-10 11:33:34 -0300

    getting the env config

--- playwright.config.ts
+++ playwright.config.ts
@@ -77,10 +77,18 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:8080',
-    cwd: './apps/web',
-    reuseExistingServer: !process.env.CI,
-  },
+  webServer: [
+    {
+      command: 'npm run dev',
+      url: 'http://localhost:8080',
+      cwd: './apps/web',
+      reuseExistingServer: !process.env.CI,
+    },
+    {
+      command: 'npm run dev',
+      url: 'http://localhost:3333',
+      cwd: './apps/api',
+      reuseExistingServer: !process.env.CI,
+    },
+  ],
 });
